0d3a41ca53e73bfb4a54e68e7c0aab06db3307f5
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-04-21 05:45:01 -0600

    Add AuthTest

--- config/test.exs
+++ config/test.exs
@@ -1,13 +1,16 @@
 use Mix.Config
 
+config :comeonin, :bcrypt_log_rounds, 4
+config :comeonin, :pbkdf2, 1
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :rumbl, Rumbl.Endpoint,
   http: [port: 4001],
   server: false
-
-# Print only warnings and errors during test
-config :logger, level: :warn
 
 # Configure your database
 config :rumbl, Rumbl.Repo,
